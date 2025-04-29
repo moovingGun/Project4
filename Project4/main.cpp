7a7216e534d9bcd63160642ee7abf01bc748e621
moovinggun <jyy9188@naver.com>
2025-04-29 11:25:28 +0900

    Initial Commit

---
+++ Project4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Project4
+//
+//  Created by 이동건 on 4/29/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
